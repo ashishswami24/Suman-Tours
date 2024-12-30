35564395dade78732e8a36d806a1563778e9bfbb
ashishswami24 <ashishkumarswami@gmail.com>
2024-12-30 18:13:41 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+const slides = document.querySelector('.slides');
+let start, change;
+
+slides.addEventListener('dragstart', (e) => {
+    start = e.clientX;
+}, false);
+
+slides.add[_{{{CITATION{{{_1{](https://github.com/tejasparkar/andeverywherewebsite/tree/18aedd59db29c5e23e1a8623807bca92ab4d55c3/resources%2Fjs%2Fcomponents%2FHeader.js)[_{{{CITATION{{{_2{](https://github.com/GaurabAryal/Fungeons-Web/tree/ff0103a65ed8373c0d519f1c3144882b1c55759a/index.php)
